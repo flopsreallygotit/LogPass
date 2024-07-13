c493f0d1cd02b6e4b9892c5a927695e755f38e7c
Egor Sheviakhov <sheviakhov.ei@star.intra.ispras.ru>
2024-07-13 19:28:20 +0300

    Added log_funcs object compiling

--- lib/log_funcs.cpp
+++ lib/log_funcs.cpp
@@ -1,13 +1,7 @@
-#include <fstream>
 #include <iostream>
 
-#include <nlohmann/json.hpp>
-using json = nlohmann::json;
+extern "C" void log_function_init() { std::cout << "~~LOG~INIT~~" << std::endl; }
 
-const char log_filename[] = "log_pass.json";
+extern "C" void log_function_call(char *function) { std::cout << "~~LOG~CALL~~: `" << function << "`" << std::endl; }
 
-extern "C" void log_function_init() { std::cout << "~~LOG INIT~~" << std::endl; }
-
-extern "C" void log_function_call(char *function) { std::cout << "~~LOG CALL~~: `" << function << "`" << std::endl; }
-
-extern "C" void log_function_deinit() { std::cout << "~~LOG DEINIT~~" << std::endl; }
+extern "C" void log_function_deinit() { std::cout << "~~LOG~DEINIT~~" << std::endl; }
