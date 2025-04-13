ba59e5c5c8ade4a51ecf6e689f3cf547a874a2fe
flops <flopsreallygotit@gmail.com>
2025-04-13 21:50:54 +0300

    Add log filename

--- lib/log_funcs.cpp
+++ lib/log_funcs.cpp
@@ -3,6 +3,8 @@
 
 class LoggerSingleton {
 private:
+    const std::string log_filename = "logger.log";
+
     std::ofstream output;
 
     static LoggerSingleton instance;
@@ -14,6 +16,7 @@ public:
 
     static LoggerSingleton &get_instance() { return instance; }
     std::ofstream          &get_output() { return output; }
+    const std::string      &get_filename() { return log_filename; }
 };
 
 template<typename T>
@@ -32,15 +35,17 @@ LoggerSingleton LoggerSingleton::instance;
 
 extern "C" void log_function_init() {
     LoggerSingleton &logger = LoggerSingleton::get_instance();
+    const std::string log_filename = logger.get_filename();
 
     std::ofstream &output = logger.get_output();
-    output.open("logger.log", std::ios_base::app);
+    output.open(log_filename, std::ios_base::app);
 }
 
 extern "C" void log_function_call(const char *function) {
     LoggerSingleton &logger = LoggerSingleton::get_instance();
 
-    logger << function;
+    std::ofstream &output = logger.get_output();
+    output << function << std::endl;
 }
 
 extern "C" void log_function_deinit() {
